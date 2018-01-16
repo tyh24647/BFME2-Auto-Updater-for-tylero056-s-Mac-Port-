f7017c58358fc8094865a208607b523f57a1d4dd
Tyler Hostager <tyhostager@gmail.com>
2018-01-15 22:56:41 -0600

    First push

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BFME2 Mega Update Installer
+//
+//  Created by Tyler hostager on 1/14/18.
+//  Copyright © 2018 Tyler hostager. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BFME2 Mega Update Installer
+//
+//  Created by Tyler hostager on 1/14/18.
+//  Copyright © 2018 Tyler hostager. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
